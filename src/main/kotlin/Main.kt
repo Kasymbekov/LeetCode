02a01031d826a7571bb997f034b7e98c061f95cf
Nursultan <nkasymbekov99@gmail.com>
2024-02-21 12:55:48 +0600

    implementaion changes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,50 +10,41 @@ fun romanToInt(s: String): Int {
     var i = 0
     while (i < s.length) {
         when (s[i].toString()) {
-//            "IV" -> result += 4
-//            "IX" -> result += 9
-//            "XL" -> result += 40
-//            "XC" -> result += 90
-//            "CD" -> result += 400
-//            "CM" -> result += 900
             "I" -> {
-                if (i != s.length - 1 && s[i + 1] == 'V') {
+                if (i != s.length -1 && s[i + 1] == 'V') {
                     result += 4
                     i++
-                } else if (s[i + 1] == 'X') {
+                } else if (i != s.length -1 && s[i + 1] == 'X') {
                     result += 9
                     i++
                 } else {
                     result += 1
                 }
             }
-
             "V" -> result += 5
             "X" -> {
-                if (s[i + 1] == 'L') {
+                if (i != s.length -1 && s[i + 1] == 'L') {
                     result += 40
                     i++
-                } else if (s[i + 1] == 'C') {
+                } else if (i != s.length -1 && s[i + 1] == 'C') {
                     result += 90
                     i++
                 } else {
                     result += 10
                 }
             }
-
             "L" -> result += 50
             "C" -> {
-                if (s[i + 1] == 'D') {
+                if (i != s.length -1 && s[i + 1] == 'D') {
                     result += 400
                     i++
-                } else if (s[i + 1] == 'M') {
+                } else if (i != s.length -1 && s[i + 1] == 'M') {
                     result += 900
                     i++
                 } else {
                     result += 100
                 }
             }
-
             "D" -> result += 500
             "M" -> result += 1000
         }
